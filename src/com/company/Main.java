0c0170dd1b1057e38d1b86166e3b73fe375949cb
Piromanski <hard.style@abv.bg>
2023-05-18 20:17:52 +0300

    First File Committed

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Teacher teacher1 = new Teacher("Georgi","Ivanov",25,"");
+        Student student1 = new Student("Atanas","Petrov",19,"","988981236");
+
+        CourseInstance courseInstance1 = new CourseInstance("Programing with Java",800,teacher1);
+        courseInstance1.enroll(student1);
+
+
+
+        System.out.print(courseInstance1);
+
+
+
+
+
+    }
+}
